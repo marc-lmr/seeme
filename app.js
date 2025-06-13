8c6f4d19220aac25ed5f16662869f48a6a311deb
marc-lmr <ma.lamorlette1@gmail.com>
2025-06-13 14:22:01 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
+// Configuration Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyC2kmIOknqnbY7nzLMvyyoO7i4fMymeM80",
+  apiKey: "AIzaSyC2kmIOknqnbY7nzLMvyy0O7i4fMymeM80",
   authDomain: "seeme-30ed9.firebaseapp.com",
   projectId: "seeme-30ed9",
-  storageBucket: "seeme-30ed9.firebasestorage.app",
+  storageBucket: "seeme-30ed9.appspot.com",
   messagingSenderId: "896639210596",
   appId: "1:896639210596:web:a127630ede855b6f243b71",
   measurementId: "G-22NC8FHQSE"
+};
+
+// Initialiser Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
+// Rendre les fonctions accessibles depuis le HTML
+
+window.signUp = function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  auth.createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      alert("Inscription réussie !");
+    })
+    .catch((error) => {
+      alert("Erreur : " + error.message);
+    });
+};
+
+window.signIn = function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  auth.signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      alert("Connexion réussie !");
+    })
+    .catch((error) => {
+      alert("Erreur : " + error.message);
+    });
+};
+
+window.signOut = function () {
+  auth.signOut().then(() => {
+    alert("Déconnexion réussie !");
+  });
 };
