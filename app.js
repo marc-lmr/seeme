5ae9df8e7e607386e7467fe6736c5c35a0a4be7b
marc-lmr <ma.lamorlette1@gmail.com>
2025-06-13 14:36:37 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // Configuration Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyC2kmIOknqnbY7nzLMvyy0O7i4fMymeM80",
+  apiKey: "AIzaSyC2kmIOknqnbY7nzLMvyyoO7i4fMymeM80",
   authDomain: "seeme-30ed9.firebaseapp.com",
   projectId: "seeme-30ed9",
-  storageBucket: "seeme-30ed9.appspot.com",
+  storageBucket: "seeme-30ed9.firebasestorage.app",
   messagingSenderId: "896639210596",
   appId: "1:896639210596:web:a127630ede855b6f243b71",
   measurementId: "G-22NC8FHQSE"
